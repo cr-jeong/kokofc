f6fe1cba5c9b6ec506b26eb111fa2c05eb6be5f2
cr-jeong <jeong2cr@naver.com>
2026-06-17 16:58:07 +0900

    Update kokofc.py

--- kokofc.py
+++ kokofc.py
@@ -2,8 +2,8 @@ import streamlit as st
 import random
 
 # 페이지 설정
-st.set_page_config(page_title="우리 팀 풋살 라인업 매니저", layout="centered")
-st.title("⚽ 우리 팀 풋살 라인업 매니저")
+st.set_page_config(page_title="KOKO FC 풋살 라인업 매니저", layout="centered")
+st.title("⚽ KOKO FC 풋살 라인업 매니저")
 st.caption("이름 먼저 입력 ➡️ 희망 포지션 선택 버전")
 
 # 포지션 정의
