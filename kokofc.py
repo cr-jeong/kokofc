e6f1dc963548a6f46d73481bac9dcda80e6ac8b0
cr-jeong <jeong2cr@naver.com>
2026-06-19 15:15:12 +0900

    Update kokofc.py

--- kokofc.py
+++ kokofc.py
@@ -151,7 +151,7 @@ col1, col2 = st.columns([1, 6])
 with col1:
     try:
         img = Image.open("koko_logo.png")
-        st.image(img, width=130) 
+        st.image(img, width=100) 
     except FileNotFoundError:
         st.title("⚽")
 
